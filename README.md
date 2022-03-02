adc75ceb1097fcc890246e043089eed587b9e604
Tweeticoats <Tweeticoats@github.com>
2022-03-02 15:15:03 +1030

    This is a tool for generating json files for xbvr based on metadata in stashdb.org. This version should have enough to connect to stashdb and generate a json file but this needs testing

---
+++ README.md
@@ -0,0 +1,6 @@
+# stashdb2xbvr
+This tool generates json import files for xbvr based on metadata from stashdb.org
+Stashdb.org is a user edited metadata database from the stash porn manager to create a shared database of metadata and file hashes.
+For instructions in using stashdb see the discord used by stash.
+
+
---
+++ stashdb2xbvr.py
@@ -0,0 +1,191 @@
+import os
+import requests
+import json
+import datetime
+import re
+from pinatapy import PinataPy
+
+headers = {
+    "Accept-Encoding": "gzip, deflate, br",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Connection": "keep-alive",
+    "DNT": "1"
+}
+api_url='https://stashdb.org/graphql'
+
+if os.getenv('API_KEY'):
+    headers['ApiKey']=os.getenv('API_KEY')
+
+
+def __callGraphQL(query, variables=None):
+    json = {}
+    json['query'] = query
+    if variables != None:
+        json['variables'] = variables
+
+    # handle cookies
+    response = requests.post(api_url, json=json, headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()
+        if result.get("error", None):
+            for error in result["error"]["errors"]:
+                raise Exception("GraphQL error: {}".format(error))
+        if result.get("data", None):
+            return result.get("data")
+    else:
+        raise Exception(
+            "GraphQL query failed:{} - {}. Query: {}. Variables: {}".format(response.status_code, response.content,
+                                                                            query, variables))
+
+
+def getScenes(studio_id,page=1,per_page=100):
+    query="""query  queryScenes($input: SceneQueryInput!) {
+  queryScenes(input: $input) {
+    count
+   scenes{
+    id
+    title
+    details
+    date
+    urls{
+      url
+    }
+    studio{
+      id
+      name
+      urls{
+        url
+      }
+    }
+    tags{
+      id
+      name
+    }
+    images{
+      id
+      url
+      width
+      height
+    }
+    performers{
+      performer{
+        name
+        id
+      }
+    }
+    duration
+    code
+  }
+  }
+}"""
+
+    variables={
+        "input": {
+            "studios": {
+                "modifier": "EQUALS",
+                "value": [studio_id]
+            },
+            "page": page,
+            "per_page": per_page,
+            "sort": "DATE"
+        }
+    }
+    result = __callGraphQL(query, variables)
+    return result
+
+
+def generateJson(studio_id):
+    data = {}
+    data["timestamp"] = datetime.datetime.now().isoformat() + "Z"
+    data["bundleVersion"] = "1"
+    res=[]
+    scenes_list=[]
+    page=1
+    per_page=100
+    res=getScenes(studio_id,page,per_page)
+    index=1
+    if res is not None:
+        while  (page-1)*per_page < res['queryScenes']['count']:
+            print("Processing... "+str((page-1)*per_page)+ "-"+str(page*per_page)+"  " +str(res['queryScenes']['count']))
+            for s in res['queryScenes']['scenes']:
+                index = index + 1
+                r = {}
+                r["_id"] = str(index)
+                r["scene_id"] = s["id"]
+                r["scene_type"] = "VR"
+                r["title"] = s["title"]
+                if "studio" in s:
+                    r["studio"] = s["studio"]["name"]
+                r["gallery"] = None
+                tags = []
+                if "tags" in s:
+                    for t in s["tags"]:
+                        tags.append(t["name"])
+                r["tags"] = tags
+
+                performer = []
+                if "performers" in s:
+                    for t in s["performers"]:
+                        performer.append(t["performer"]["name"])
+                r["cast"] = performer
+#                path = s["path"][s["path"].rindex('/') + 1:]
+
+                r["synopsis"] = s["details"]
+                r["released"] = s["date"]
+
+
+
+                for url in s["urls"]:
+                    site_url=None
+                    # check if the url matches the site url to filter out other links like indexx or iafd etc
+                    if "urls" in s["studio"]:
+                        for u in s["studio"]["urls"]:
+                            if url["url"].startswith(u["url"]):
+                                r["homepage_url"]=url["url"]
+                    reg=re.search('vrh(\d+)',url["url"])
+                    if reg:
+                        id=reg.group()[3:]
+                        r["_id"] = 'vrhush-'+id
+                        r["scene_id"] = id
+
+
+
+
+
+
+
+#                r["homepage_url"] = s["url"]
+                print(r["_id"]+" "+r["scene_id"]+" "+r["released"])
+                scenes_list.append(r)
+
+            page=page+1
+            res = getScenes(studio_id, page, per_page)
+            print("loop")
+
+    data["scenes"] = scenes_list
+    return data
+
+
+print( os.getenv('PINATA_API_KEY'))
+print(os.getenv('PINATA_SECRET_KEY'))
+
+if __name__ == '__main__':
+    id='c85a3d13-c1b9-48d0-986e-3bfceaf0afe5'
+    res=generateJson(id)
+    print(res)
+    file=id+'.json'
+
+    pinata = PinataPy( os.getenv('PINATA_API_KEY'), os.getenv('PINATA_SECRET_KEY'))
+    upload={}
+    upload["pinataMetadata"]={"name":file}
+    upload["pinataContent"]=res
+
+    status=pinata.pin_json_to_ipfs(upload)
+    print(status)
+
+#    with open(file, 'w') as outfile:
+#        json.dump(res, outfile)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
